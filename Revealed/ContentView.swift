6a060917cc9bc449049da49853bd6a5b2b5c970f
Alvin Choo <kokhong200@gmail.com>
2019-11-27 20:16:05 +0800

    Setup Xcode Project

---
+++ Revealed/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Revealed
+//
+//  Created by KokHong on 27/11/19.
+//  Copyright © 2019 Pointwelve. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
