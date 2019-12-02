d5cd033c912587c8537a947dca5347e147b7a629
Alvin Choo <kokhong200@gmail.com>
2019-12-02 22:37:09 +0800

    feat: Changed to use future. Added swiftlint and cleanup script

--- Revealed/ContentView.swift
+++ Revealed/ContentView.swift
@@ -9,13 +9,13 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        Text("Hello, World!")
-    }
+  var body: some View {
+    Text("Hello, World!")
+  }
 }
 
 struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
+  static var previews: some View {
+    ContentView()
+  }
 }
---
+++ Revealed/Network/ApolloClient+Future.swift
@@ -0,0 +1,72 @@
+//
+//  ApolloNetwork+Combine.swift
+//  Revealed
+//
+//  Created by Hong on 30/11/19.
+//  Copyright © 2019 Pointwelve. All rights reserved.
+//
+
+import Apollo
+import Combine
+import Foundation
+
+extension ApolloClient {
+  func fetchFuture<Query>(query: Query,
+                          cachePolicy: Apollo.CachePolicy = .returnCacheDataElseFetch,
+                          context: UnsafeMutableRawPointer? = nil,
+                          queue: DispatchQueue = DispatchQueue.main) -> Future<Query.Data, Error> where Query: Apollo.GraphQLQuery {
+    return Future<Query.Data, Error> { [weak self] promise in
+      self?.fetch(query: query,
+                  cachePolicy: cachePolicy,
+                  context: context,
+                  queue: queue) { result in
+        switch result {
+        case let .success(result):
+          guard (result.errors ?? []).isEmpty else {
+            if let error = result.errors?.first {
+              promise(.failure(error))
+            }
+            return
+          }
+
+          guard let data = result.data else {
+            promise(.failure(ApolloError.empty))
+            return
+          }
+          promise(.success(data))
+        case let .failure(error):
+          promise(.failure(error))
+        }
+      }
+    }
+  }
+
+  func mutateFuture<Mutation>(mutation: Mutation,
+                              cachePolicy: Apollo.CachePolicy = .returnCacheDataElseFetch,
+                              context: UnsafeMutableRawPointer? = nil,
+                              queue: DispatchQueue = DispatchQueue.main) -> Future<Mutation.Data, Error> where Mutation: Apollo.GraphQLMutation {
+    return Future<Mutation.Data, Error> { [weak self] promise in
+      self?.perform(mutation: mutation,
+                    context: context,
+                    queue: queue) { result in
+        switch result {
+        case let .success(result):
+          guard (result.errors ?? []).isEmpty else {
+            if let error = result.errors?.first {
+              promise(.failure(error))
+            }
+            return
+          }
+
+          guard let data = result.data else {
+            promise(.failure(ApolloError.empty))
+            return
+          }
+          promise(.success(data))
+        case let .failure(error):
+          promise(.failure(error))
+        }
+      }
+    }
+  }
+}
