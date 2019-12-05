41d1e10f901d443cbd40d6d3a183f48e3f65b83d
Li Hao <james_lai@ntucenterprise.sg>
2019-12-05 20:31:59 +0800

    feat: Include Auth0 SDK

--- Revealed/ContentView.swift
+++ Revealed/ContentView.swift
@@ -7,11 +7,34 @@
 //
 
 import SwiftUI
+import Auth0
 
 struct ContentView: View {
-  var body: some View {
-    Text("Hello, World!")
-  }
+    var body: some View {
+        NavigationView {
+            Button(action: auth) {
+                Text("Auth0")
+            }
+        }
+    }
+
+    private func auth() {
+        Auth0
+            .webAuth()
+            .scope("openid profile")
+            .audience("https://pointwelve.au.auth0.com/userinfo")
+            .start {
+                switch $0 {
+                case .failure(let error):
+                    // Handle the error
+                    print("Error: \(error)")
+                case .success(let credentials):
+                    // Do something with credentials e.g.: save them.
+                    // Auth0 will automatically dismiss the login page
+                    print("Credentials: \(credentials)")
+                }
+            }
+    }
 }
 
 struct ContentView_Previews: PreviewProvider {
