f99c500ab3be6b28b657bebd410995b95a20c6f4
Alvin Choo <kokhong200@gmail.com>
2019-12-13 00:09:38 +0800

    feat: Added OHHTTPStubs
    # Conflicts:
    #	Revealed.xcodeproj/project.pbxproj

---
+++ RevealedTests/XCTestCase+Global.swift
@@ -0,0 +1,15 @@
+//
+//  XCTestCase+Global.swift
+//  RevealedTests
+//
+//  Created by Hong on 12/12/19.
+//  Copyright © 2019 Pointwelve. All rights reserved.
+//
+
+import XCTest
+
+extension XCTestCase {
+  var timeout: Double {
+    return 5.0
+  }
+}
