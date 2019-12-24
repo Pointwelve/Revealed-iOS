44847b188a8fe70d5c65a0b40909a62aa44694c8
Lai Li Hao <lihao6485@gmail.com>
2019-12-24 23:45:11 +0800

    feat: get started view basic styling

--- Revealed/Features/Auth/Get Started/GetStartedView.swift
+++ Revealed/Features/Auth/Get Started/GetStartedView.swift
@@ -14,30 +14,36 @@ struct GetStartedView<Page: View>: View {
 
   @State var currentPage = 0
 
-  @State var goToHome = false
-
   init(_ views: [Page]) {
     viewControllers = views.map { UIHostingController(rootView: $0) }
   }
 
   var body: some View {
-    ZStack(alignment: Alignment(horizontal: .center, vertical: .bottom)) {
-      GetStartedPageViewController(controllers: viewControllers, currentPage: $currentPage)
+    VStack(alignment: .leading) {
+      Image("Icon")
 
-      VStack {
+      ZStack(alignment: Alignment(horizontal: .center, vertical: .bottom)) {
+        GetStartedPageViewController(controllers: viewControllers, currentPage: $currentPage)
+
         GetStartedPageControl(numberOfPages: viewControllers.count, currentPage: $currentPage)
+      }
+      .padding(EdgeInsets(top: 16, leading: 0, bottom: 16, trailing: 0))
 
-        Button(action: auth) {
-          Text("Get started")
-        }
+      Button(action: auth) {
+        Text("Get Started")
+          .frame(minWidth: 0, maxWidth: .infinity, minHeight: 56, idealHeight: 56, maxHeight: 56)
+          .foregroundColor(.white)
+          .background(Color(red: 35 / 255, green: 93 / 255, blue: 227 / 255))
+          .cornerRadius(10)
       }
     }
+    .padding(EdgeInsets(top: 32, leading: 32, bottom: 0, trailing: 32))
   }
 
   private func auth() {
     Auth0
       .webAuth()
-      .scope("openid profile offline_access")
+      .scope("openid profile offline_access email")
       .audience("https://pointwelve.au.auth0.com/userinfo")
       .start {
         switch $0 {
@@ -46,7 +52,6 @@ struct GetStartedView<Page: View>: View {
           print("Error: \(error)")
         case let .success(credentials):
           AuthService.shared.store(credentials: credentials)
-          self.goToHome = true
         }
       }
   }
