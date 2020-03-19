9ae06a5ba0877fb50282f89e558aed677ae5c94c
Jun <missingems@gmail.com>
2020-03-19 11:51:43 +0800

    Created Spacing with predefined values

---
+++ Revealed/Design Components/Spacing.swift
@@ -0,0 +1,11 @@
+import CoreGraphics
+
+extension CGFloat {
+  static let xxsmall = CGFloat(3.0)
+  static let xsmall = CGFloat(5.0)
+  static let small = CGFloat(8.0)
+  static let medium = CGFloat(11.0)
+  static let big = CGFloat(13.0)
+  static let padding = CGFloat(16.0)
+  static let large = CGFloat(21.0)
+}
